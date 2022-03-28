00e709f2a83c141e87ee4afaa725966f2acaeb8b
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-03-29 01:23:19 +0700

    setting fresh dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Route::get('/', [BlogController::class, 'allPost']);
+Route::get('/admin/dashboard', function () {
+    return view('admin.index');
+});
