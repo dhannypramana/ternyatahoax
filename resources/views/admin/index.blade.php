7e5548f8359b0743c57323a7cbabb4ab6ca28cca
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-03-29 01:35:25 +0700

    add dummy table

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -372,6 +372,34 @@
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
 
+                    {{-- My Code Goes Here --}}
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>No.</th>
+                            <th>Judul Hoaks</th>
+                            <th>Tanggal Ajuan</th>
+                            <th>Review Hoaks</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td><a href="#">Hacker WhatsApp</a></td>
+                            <td>20 Maret 2022</td>
+                            <td>
+                                <button class="btn btn-success">Fakta</button>
+                                <button class="btn btn-danger">Hoax</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td><a href="">Hemat Token Listrik</a></td>
+                            <td>22 Maret 2022</td>
+                            <td>
+                                <button class="btn btn-success">Fakta</button>
+                                <button class="btn btn-danger">Hoax</button>
+                            </td>
+                        </tr>
+                    </table>
+
                     {{-- <div class="helper-desk">
                         <!-- Content Row -->
                         <div class="row">
