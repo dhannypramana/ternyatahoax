8b9164a564b251050effcc7a6fe1330459043a87
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-04-03 00:38:22 +0700

    type class name in routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use llluminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Collection;
 
 /*
 |--------------------------------------------------------------------------
