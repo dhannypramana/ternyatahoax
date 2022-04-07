35fe8fbbbaaafe4a02fb7905035ba8b6abc3a12c
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-04-08 00:33:05 +0700

    delete printing func

--- resources/views/admin/unreviewed.blade.php
+++ resources/views/admin/unreviewed.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('generate_reports')
-    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
 @endsection
 
 @section('container')
